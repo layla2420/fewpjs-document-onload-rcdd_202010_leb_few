5990cee7b9397e43d5b0baff4258f9bd7d5c0c61
layla  hammoud <laylahammoud63@gmail.com>
2020-12-31 14:05:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ document.addEventListener( "DOMContentLoaded", function () {
   updateDOM()
 } );
 function updateDOM() {
-  document.getElementById( "text" )
-    .innerHTML = "This is really cool!";
+  document.getElementById( "text" ).innerHTML = "This is really cool!";
