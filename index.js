db812fd6ec86377c125dbb7b44d83eb6719232e6
layla  hammoud <laylahammoud63@gmail.com>
2020-12-31 13:53:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-var para=  document.getElementByID(text)
+let para=  document.getElementByID("text")
 para.innerHTML= "This is really cool!"
 });
